ab16b1c43804b61b9db5ce53d8751cf2d154d4c7
Brayden O'Neal <braydenoneal@protonmail.com>
2025-04-02 14:16:54 -0500

    Add webcam tracking

--- start_kit/landmarks/hand_tracking_cam.py
+++ start_kit/landmarks/hand_tracking_cam.py
@@ -1,4 +1,3 @@
-# code adapted from https://github.com/prashver/hand-landmark-recognition-using-mediapipe
 import cv2 as cv
 import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.pose as mp_pose
@@ -23,41 +22,34 @@ face = mp_face.FaceMesh(
     min_detection_confidence=0.5
 )
 
-# Open the video file
-VIDEO_FILE = '68011.mp4'  # Replace 'video.mp4' with the path to your video file
-cap = cv.VideoCapture(VIDEO_FILE)
+# Set the desired resolution (e.g., 1280x720)
+width, height = 1280, 720
 
-# Get the video frame width and height
-frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+cam = cv.VideoCapture(0)
+cam.set(3, width)  # Set the width
+cam.set(4, height)  # Set the height
 
-# Define the codec and create VideoWriter object
-fourcc = cv.VideoWriter_fourcc(*'mp4v')  # Codec for saving the video
-out = cv.VideoWriter('68011-out.mp4', fourcc, 15.0, (frame_width, frame_height))
+while cam.isOpened():
+    success, frame_rgb = cam.read()
+    if not success:
+        print("Camera Frame not available")
+        continue
 
-while cap.isOpened():
-    # Read a frame from the video
-    ret, frame = cap.read()
-    if not ret:
-        break
+    # Convert image to RGB format
+    frame_rgb = cv.cvtColor(frame_rgb, cv.COLOR_BGR2RGB)
 
-    # Resize the frame to fit the display window
-    frame = cv.resize(frame, (1280, 720))  # Adjust the dimensions as needed
-
-    # Convert the frame to RGB format
-    frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-
-    # Process the frame with MediaPipe Hands
     results = hands.process(frame_rgb)
     pose_results = pose.process(frame_rgb)
     face_results = face.process(frame_rgb)
 
-    POSE_CONNECTIONS = (
-        [(11, 12), (11, 13), (13, 15), (12, 14), (14, 16), (11, 23), (12, 24), (23, 24), (23, 25), (24, 26), (25, 27),
-         (26, 28), (27, 29), (28, 30), (29, 31), (30, 32), (27, 31), (28, 32)])
+    POSE_CONNECTIONS = frozenset([
+        (11, 12), (11, 13), (13, 15), (12, 14), (14, 16), (11, 23), (12, 24), (23, 24), (23, 25), (24, 26), (25, 27),
+        (26, 28), (27, 29), (28, 30), (29, 31), (30, 32), (27, 31), (28, 32)
+    ])
 
-    # override the video with zeros to create the black background
-    frame = np.zeros_like(frame)
+    # Convert image to RGB format
+    frame_rgb = cv.cvtColor(frame_rgb, cv.COLOR_RGB2BGR)
+    frame = np.zeros_like(frame_rgb)
 
     if pose_results.pose_landmarks:
         drawing.draw_landmarks(
@@ -77,7 +69,7 @@ while cap.isOpened():
                 mp_face.FACEMESH_LEFT_EYEBROW,
                 mp_face.FACEMESH_RIGHT_EYE,
                 mp_face.FACEMESH_RIGHT_EYEBROW,
-                # mp_face.FACEMESH_NOSE,
+                mp_face.FACEMESH_NOSE,
                 # mp_face.FACEMESH_TESSELATION
             ]:
                 drawing.draw_landmarks(
@@ -98,13 +90,9 @@ while cap.isOpened():
                 is_drawing_landmarks=False
             )
 
-    # Display the annotated frame
-    cv.imshow('Hand Tracking', frame)
+    cv.imshow("Show Video", cv.flip(frame, 1))
 
-    # Press 'q' to exit the loop
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(20) & 0xff == ord('q'):
         break
 
-# Release the video capture object and close all windows
-cap.release()
-cv.destroyAllWindows()
+cam.release()
